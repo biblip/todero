ce863fd06bfdeef85e7f96482e808a3c088945ba
author <author@unknown.com>
2024-11-11 11:13:50 -0500

    Improve Exclusions on Common module & Clean CliMain

--- cli/src/main/java/com/social100/todero/CliMain.java
+++ cli/src/main/java/com/social100/todero/CliMain.java
@@ -31,7 +31,6 @@ public class CliMain {
             appConfig = yamlMapper.readValue(new File("config.yaml"), AppConfig.class);
             Cli cli = new Cli(appConfig);
             cli.execute(args);
-            cli.wait(60000);
         } catch (NullPointerException e) {
             System.out.println(e.getMessage());
         } catch (StreamReadException e) {
@@ -40,35 +39,6 @@ public class CliMain {
             System.out.println(e.getMessage());
         } catch (IOException e) {
             System.out.println(e.getMessage());
-        } catch (InterruptedException e) {
-            System.out.println(e.getMessage());
         }
     }
-
-    /*
-    public static void main(String[] args) throws IOException {
-        boolean isIDE = System.getProperty("java.class.path").contains("idea_rt.jar");
-        System.out.println(System.getProperty("java.class.path"));
-        if (isIDE) {
-            // Use Scanner when running inside an IDE
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter input Scanner> ");
-            String line = scanner.nextLine();
-            System.out.println("You entered: " + line);
-        } else {
-            // Use JLine when running outside the IDE
-            Terminal terminal = TerminalBuilder.builder()
-                    .jna(false)
-                    .jansi(true)
-                    .system(true)
-                    .build();
-
-            LineReader lineReader = LineReaderBuilder.builder()
-                    .terminal(terminal)
-                    .build();
-
-            String line = lineReader.readLine("Enter input> ");
-            System.out.println("You entered: " + line);
-        }
-    }*/
 }
